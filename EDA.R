a64af8d73cc9faaf12ef587c1b0327b31f2ea8d0
JosefinaAraya <araya.tapia.j@gmail.com>
2019-11-08 22:45:53 -0300

    EDA con histogramas

--- EDA.R
+++ EDA.R
@@ -23,3 +23,34 @@ plot(orangeec$GDP.PC ~ orangeec$Creat.Ind...GDP,
      xlab="Aporte Economia Naranja al PIB",
      ylab = "PIB Per Capita",
      main="Relacion Economia Naranja y PIB per Capita ")
+
+
+##EDA Histogramas
+
+gplot(mtcars$hp,
+      geom="histogram",
+      xlab="Caballos de Fuerza",
+      main="Carros Segun Caballos de Fuerza")
+
+
+
+ggplot(mtcars,aes(x=hp))+
+       geom_histogram(binwidth = 30)+
+       labs(x="Caballos de Fuerza",y="Cantidad de Autos",
+       title="Caballos de Fuerza en Autos Seleecionados")+
+       theme(legend.position = "none")+
+       theme(panel.background = element_blank(),
+             panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank())
+
+ggplot()+geom_histogram(data = mtcars,
+                        aes(x=hp),fill="Blue",color="Red",
+                        binwidth =20)+
+  labs(x="Caballos de Fuerza",y="Cantidad de Autos",
+       title="Caballos de Fuerza en Autos Seleecionados")+
+  
+  xlim(c(80,280))+
+  theme(legend.position = "none")+
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
